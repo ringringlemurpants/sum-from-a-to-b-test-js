894c4ce5a41c841a8ed6bdcf7afb1ff19a21cc55
Ghabe Bossin <gee.bossin@gmail.com>
2019-02-08 00:22:20 +0000

    recrursive complete

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
-// function sum(fromN, toN) {
-//   // Sum all the values from fromN up to toN
-//   // base case
-//   if (fromN === toN) {
-//     return toN;
-//   }
-//   let current = sum((fromN + 1), toN);
-//   return current;
-// }
-
 function sum(fromN, toN) {
-  let current = (fromN + 1);
-  if (fromN !== toN) {
-    if (current < toN) {
-      
-    }
-    // let current = sum((fromN + 1), toN);
-    return sum((fromN + 1), toN);
-    console.log(current);
-    return current;
-  } else {
+  let current = fromN;
+  if (fromN === toN) {
     return current;
   }
+    current = current + 1;
+    return fromN + sum(current , toN);
 }
-
-console.log("the function:", sum(3,7));
-
-
 module.exports = sum;
 
 
